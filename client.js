eb42a076963dbf8acb8eed7d7e687be09d992986
Kayode Adebanjo <kay.adebanjo@gmail.com>
2021-12-11 20:53:58 -0600

    grab input from the DOM

--- client.js
+++ client.js
@@ -11,4 +11,22 @@ function onAddEmployee(action) {
     action.preventDefault();
 
     console.log('add employee');
+
+    let firstName = $('#firstNameInput').val();
+    let lastName = $('#lastNameInput').val();
+    let id = $('#idInput').val();
+    let title = $('#titleImput').val();
+    let annualSalary = $('#annualSalaryInput').val();
+
+    let employee = {
+        firstName: firstName,
+        lastName: lastName,
+        id: id,
+        title: title,
+        annualSalary: annualSalary
+    }
+
+    console.log('New employee salary', employee);
+
+
 }
